d78f41cd0138baff530a10cbc124039dd2bc56f1
lcamilo <lhenriquegomescamilo@gmail.com>
2019-04-28 00:57:19 -0300

    Ajustando porta do jenkinst

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -41,7 +41,7 @@ podTemplate(
                 ENVIRONMENT = 'development'
                 HELM_DEPLOY_NAME = ENVIRONMENT + "-" + MICROSERVICE_NAME
                 IMAGE_POSFIX = "-RC"
-                NODE_PORT = "30013"
+                NODE_PORT = "30014"
                 echo "Ambiente development"
                 echo "CURRENT BRANCH ${GIT_BRANCH}"
             } else {
